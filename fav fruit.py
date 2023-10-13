12ec16d7c543fb92c7734ed2b709cb2a228bfc24
Mehar Parmar <147578412+meharparmarr@users.noreply.github.com>
2023-10-13 21:53:28 +0530

    Add files via upload

---
+++ fav fruit.py
@@ -0,0 +1,12 @@
+favorite_fruits = input("enter the fruit name")
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
+if "apple" in favorite_fruits:
+    print("You really like apples!")
+if "mango" in favorite_fruits:
+    print("You really like mangoes!")
+if "orange" in favorite_fruits:
+    print("You really like oranges!")
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis!")
+    
