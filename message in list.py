82b97606eb59c19328f46fdad2d4eef98051e2be
Mehar Parmar <147578412+meharparmarr@users.noreply.github.com>
2023-10-13 21:58:52 +0530

    Add files via upload

---
+++ message in list.py
@@ -0,0 +1,7 @@
+# Create a list of your friends' names
+names = ["Alice", "Bob", "Charlie", "David", "Eve"]
+
+# Print a personalized message for each person
+for name in names:
+    message = f"Hello, {name}! I hope you're having a great day."
+    print(message)
